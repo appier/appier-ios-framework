4be9f0396e0688ffb015cf1e00a1f5228d06d632
jenkins-aiqua-ficus <jenkins-aiqua-ficus@appier.com>
2020-08-18 13:18:16 +0800

    automated commit by jenkins

--- Appier.framework/Headers/Appier.h
+++ Appier.framework/Headers/Appier.h
@@ -15,16 +15,3 @@ FOUNDATION_EXPORT double AppierVersionNumber;
 FOUNDATION_EXPORT const unsigned char AppierVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like
-
-
-#import <Appier/QGSdk.h>
-#import <Appier/QGWKWebView.h>
-#import <Appier/QGInbox.h>
-#import <Appier/AIQP.h>
-#import <Appier/AIQP+ReactNative.h>
-#import <Appier/QGNotificationSdk.h>
-#import <Appier/QGNotificationService.h>
-#import <Appier/QGNotificationContentViewController.h>
-#import <Appier/AIDManager.h>
-#import <Appier/AIDPageType.h>
-#import <Appier/AIDConversionInfo.h>
