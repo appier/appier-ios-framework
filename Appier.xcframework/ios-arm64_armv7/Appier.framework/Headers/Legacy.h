79a2981e8daf40458bc8cd204c63d645269e04f7
jenkins-aiqua-ficus <jenkins-aiqua-ficus@appier.com>
2021-03-12 15:11:50 +0800

    [TEST] Release v7.8.999

---
+++ Appier.xcframework/ios-arm64_armv7/Appier.framework/Headers/Legacy.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+extern NSString * const AIDPageType_Uninterested DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeUninterested'");
+extern NSString * const AIDPageType_Top DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeTop'");
+extern NSString * const AIDPageType_Category DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeCategory'");
+extern NSString * const AIDPageType_Search DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeSearch'");
+extern NSString * const AIDPageType_Item DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeItem'");
+extern NSString * const AIDPageType_Cart DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeCart'");
+extern NSString * const AIDPageType_CartForm DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeCartForm'");
+extern NSString * const AIDPageType_Conversion DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeConversion'");
+extern NSString * const AIDPageType_MyPage DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeMyPage'");
+extern NSString * const AIDPageType_Login DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeLogin'");
+extern NSString * const AIDPageType_RegistrationForm DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeRegistrationForm'");
+extern NSString * const AIDPageType_Registration DEPRECATED_MSG_ATTRIBUTE("renamed to 'AIDPageTypeRegistration'");
+
+@class AIDConversion;
+@compatibility_alias AIDConversionInfo AIDConversion;
--- Appier.xcframework/ios-arm64_i386_x86_64-simulator/Appier.framework/Headers/Appier.h
+++ Appier.xcframework/ios-arm64_i386_x86_64-simulator/Appier.framework/Headers/Appier.h
@@ -1,29 +1,20 @@
-//
-//  Appier.h
-//  Appier
-//
-//  Copyright © 2020 Appier. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-//! Project version number for Appier.
 FOUNDATION_EXPORT double AppierVersionNumber;
-
-//! Project version string for Appier.
 FOUNDATION_EXPORT const unsigned char AppierVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like
+// AiDeal public headers
+#import <Appier/AIDPage.h>
+#import <Appier/Legacy.h>
 
-
+// AIQUA public headers
 #import <Appier/QGSdk.h>
 #import <Appier/QGWKWebView.h>
 #import <Appier/QGInbox.h>
 #import <Appier/AIQP.h>
 #import <Appier/AIQP+ReactNative.h>
+
+// AIQUA Notification public headers
 #import <Appier/QGNotificationSdk.h>
 #import <Appier/QGNotificationService.h>
 #import <Appier/QGNotificationContentViewController.h>
-#import <Appier/AIDManager.h>
-#import <Appier/AIDPageType.h>
-#import <Appier/AIDConversionInfo.h>
