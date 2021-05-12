bbba6af1221ce212856789b3a441e1b65629deb4
Greg de J <greg@nckh.com>
2021-05-12 18:18:28 +0900

    Release v7.10.1

--- AppierExtension.xcframework/AppierExtension.xcframework/ios-arm64_armv7/AppierExtension.framework/Headers/AppierExtension.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double AppierExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char AppierExtensionVersionString[];
-
-#import <AppierExtension/QGNotificationSdk.h>
-#import <AppierExtension/QGNotificationService.h>
-#import <AppierExtension/QGNotificationContentViewController.h>
